dcef9a19f58f8eb19bcb26faf76055172b7c6720
Rolling_meatballs <yangfan__8123@163.com>
2020-04-28 16:39:45 +0800

    assignment02 finish

--- lesson01/assignment02.cpp
+++ lesson01/assignment02.cpp
@@ -16,208 +16,321 @@ log(Args... args) {
     cout << endl;
 }
 
-float
-sum(vector<float> &array) {
-    // Seting a s to store array sum
-    float s = 0;
-    // The return value of .size() is size_t
-    // so the i is size_t (same as int)
-    size_t i = 0;
-    // array.size() give a measurement of vector
-    while (i < array.size()) {
-        float n = array[i];
-        s = s + n;
-        i = i + 1;
-    }
-    return s;
-}
 
 void
 ensure(bool condition, const string &message) {
     if (condition) {
         log("test succeed");
     } else {
+        log("test fail", message);
+    }
+}
+
+int
+find(const string &s1, char s2) {
+    // 返回 s2 在 s1 中的下标, 从 0 开始, 如果不存在则返回 -1
+    size_t i = 0;
+    int index = -1;
+    while (i < s1.size()) {
+        if (s2 == s1[i]) {
+            index = i;
+            break;
+        }
+        i = i + 1;
+    }
+    return index;
+}
+
+void
+testFind() {
+    ensure(find("hello", 'a') == -1, "find 1");
+    ensure(find("hello", 'e') == 1, "find 2");
+    ensure(find("hello", 'l') == 2, "find 3");
+}
+
+string
+lowercase(const string &s) {
+    // 这里是两个字符串, 包含了大写字母和小写字母
+    // 用 const 修饰是因为它们并不会被修改
+    const string lower = "abcdefghijklmnopqrstuvwxyz";
+    const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // 初始化一个空字符串
+    string result = "";
+    size_t i = 0;
+    while (i < s.size()) {
+        // 注意, 这个 find 已经帮你实现了
+        int index = find(upper, s[i]);
+        // 字符串可以用加号拼接, 不明白可以 log 一下
+        result += lower[index];
+        i += 1;
+    }
+    return result;
+}
+
+void
+testLowercase() {
+    ensure(lowercase("HELLO") == "hello", "lowercase 1");
+    ensure(lowercase("GUA") == "gua", "lowercase 2");
+}
+
+string
+uppercase(const string &s) {
+    const string lower = "abcdefghijklmnopqrstuvwxyz";
+    const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string result = "";
+    size_t i = 0;
+    while (i < s.size()) {
+        int index = find(lower, s[i]);
+        result += upper[index];
+        i += 1;
+    }
+    return result;
+}
+
+void
+testUppercase() {
+    ensure(uppercase("hello") == "HELLO", "uppercase 1");
+    ensure(uppercase("gua") == "GUA", "uppercase 2");
+}
+
+string
+lowercase1(const string &s) {
+    const string lower = "abcdefghijklmnopqrstuvwxyz";
+    const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string result = "";
+    size_t i = 0;
+    while (i < s.size()) {
+        int index = find(upper, s[i]);
+        if (index == -1) {
+            result += s[i];
+        } else {
+            result += lower[index];
+        }
+        i += 1;
+    }
+    return result;
+}
+
+void
+testLowercase1() {
+    ensure(lowercase1("heLLo") == "hello", "lowercase1 1");
+    ensure(lowercase1("gua") == "gua", "lowercase1 2");
+    ensure(lowercase1("GW") == "gw", "lowercase1 3");
+}
+
+string
+uppercase1(const string &s) {
+    const string lower = "abcdefghijklmnopqrstuvwxyz";
+    const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string result = "";
+    size_t i = 0;
+    while (i < s.size()) {
+        int index = find(lower, s[i]);
+        if (index == -1) {
+            result += s[i];
+        } else {
+            result += upper[index];
+        }
+        i += 1;
+    }
+    return result;
+}
+
+void
+testUppercase1() {
+    ensure(uppercase1("helLo") == "HELLO", "uppercase1 1");
+    ensure(uppercase1("gUA") == "GUA", "uppercase1 2");
+}
+
+string
+encode1(const string &s) {
+    const string lower = "abcdefghijklmnopqrstuvwxyz";
+    string result = "";
+    size_t i = 0;
+    while (i < s.size()) {
+        int index = find(lower, s[i]);
+        int n = index + 1;
+        if (n < lower.size()) {
+            result += lower[n];
+        } else {
+            result += lower[0];
+        }
+        i += 1;
+    }
+    // log(result);
+    return result;
+}
+
+void
+testEncode1() {
+    ensure(encode1("afz") == "bga", "encode1 1");
+    ensure(encode1("gw") == "hx", "encode1 2");
+}
+
+string
+decode1(const string &s) {
+    const string lower = "abcdefghijklmnopqrstuvwxyz";
+    string result = "";
+    size_t i = 0;
+    while (i < s.size()) {
+        int index = find(lower, s[i]);
+        int n = index - 1;
+        if (n < 0) {
+            result += "z";
+        } else {
+            result += lower[n];
+        }
+        i += 1;
+    }
+    // log(result);
+    return result;
+}
+
+void
+testDecode1() {
+    ensure(decode1("bga") == "afz", "decode1 1");
+    ensure(decode1("hx") == "gw", "decode1 2");
+}
+
+string
+encode2(const string &s, int shift) {
+    const string lower = "abcdefghijklmnopqrstuvwxyz";
+    string result = "";
+    size_t i = 0;
+    while (i < s.size()) {
+        int index = find(lower, s[i]);
+        int n = index + shift;
+        if (n < lower.size()) {
+            result += lower[n];
+        } else {
+            int m = n - 26;
+            result += lower[m];
+        }
+        i += 1;
+    }
+    // log(result);
+    return result;
+}
+
+void
+testEncode2() {
+    ensure(encode2("afz", 2) == "chb", "encode2 1");
+    ensure(encode2("gw", 3) == "jz", "encode2 2");
+}
+
+string
+decode2(const string &s, int shift) {
+    const string lower = "abcdefghijklmnopqrstuvwxyz";
+    string result = "";
+    size_t i = 0;
+    while (i < s.size()) {
+        int index = find(lower, s[i]);
+        int n = index - shift;
+        if (n < 0) {
+            int m = n + 26;
+            result += lower[m];
+        } else {
+            result += lower[n];
+        }
+        i += 1;
+    }
+    // log(result);
+    return result;
+}
+
+void
+testDecode2() {
+    ensure(decode2("chb", 2) == "afz", "decode2 1");
+    ensure(decode2("jz", 3) == "gw", "decode2 2");
+}
+
+string
+encode3(const string &s, int shift) {
+    const string lower = "abcdefghijklmnopqrstuvwxyz";
+    string result = "";
+    size_t i = 0;
+    while (i < s.size()) {
+        int index = find(lower, s[i]);
+        if (index == -1) {
+            result += s[i];
+        } else {
+            int n = index + shift;
+            if (n < lower.size()) {
+                result += lower[n];
+            } else {
+                int m = n - 26;
+                result += lower[m];
+            }
+        }
+        i += 1;
+    }
+    // log(result);
+    return result;
+}
+
+void
+testEncode3() {
+    ensure(encode3("a fz", 2) == "c hb", "encode3 1");
+    ensure(encode3("g#w", 3) == "j#z", "encode3 2");
+}
+
+string
+decode3(const string &s, int shift) {
+    const string lower = "abcdefghijklmnopqrstuvwxyz";
+    string result = "";
+    size_t i = 0;
+    while (i < s.size()) {
+        int index = find(lower, s[i]);
+        if (index == -1) {
+            result += s[i];
+        } else {
+            int n = index - shift;
+            if (n < 0) {
+                int m = n + 26;
+                result += lower[m];
+            } else {
+                result += lower[n];
+            }
+        }
+        i += 1;
+    }
+    // log(result);
+    return result;
+}
+
+void
+testDecode3() {
+    ensure(decode3("ch#b", 2) == "af#z", "decode3 1");
+    ensure(decode3("j z", 3) == "g w", "decode3 2");
+}
+
+void
+decode4() {
+    const string code = "VRPHWLPHV L ZDQW WR FKDW ZLWK BRX,EXW L KDYH QR UHDVRQ WR FKDW ZLWK BRX";
+    const string lower = "abcdefghijklmnopqrstuvwxyz";
+    string lowercode = lowercase1(code);
+    size_t i = 0;
+    while(i < 26) {
+        i += 1;
+        string message = decode3(lowercode, i);
         log(message);
     }
 }
 
-bool
-floatEqual(float a, float b) {
-    auto delta = 0.00001;
-    return a - b < delta && b - a < delta;
-}
-
-void
-testSum(void) {
-    vector<float> v1 = {1, 2, 3, 4};
-    float s1 = sum(v1);
-    ensure(floatEqual(s1, 10), "sum test error 1");
-    vector<float> v2 = {1, 2, 3, 4, 5, 6};
-    float s2 = sum(v2);
-    ensure(floatEqual(s2, 21), "sum test error 2");
-}
-
-float
-product(vector<float> &array) {
-    float s = 1;
-    size_t i = 0;
-    while (i < array.size()) {
-        float n = array[i];
-        s = s * n;
-        // log(s);
-        i = i + 1;
-    }
-    return s;
-}
-
-void
-testProduct(void) {
-    vector<float> v1 = {1, 2, 3, 4};
-    float s1 = product(v1);
-    ensure(floatEqual(s1, 24), "sum test error 3");
-    vector<float> v2 = {1, 2, 3, 4, 5, 6};
-    float s2 = product(v2);
-    ensure(floatEqual(s2, 720), "sum test error 4");
-}
-
-float
-abs1(float n) {
-    if (n < 0) {
-        n = - n;
-    }
-    return n;
-}
-
-void
-testabs1(void) {
-    //float v1 = -5;
-    float s1 = abs1(-5);
-    ensure(floatEqual(s1, 5), "sum test error 5");
-    float v2 = 10;
-    float s2 = abs1(v2);
-    ensure(floatEqual(s2, 10), "sum test error 6");
-}
-
-float
-average(vector<float> &array) {
-    float s = sum(array);
-    float n = array.size();
-    float aver = s / n;
-    return aver;
-}
-
-void
-testAverage(void) {
-    vector<float> v1 = {1, 2, 3, 4, 5};
-    float s1 = average(v1);
-    ensure(floatEqual(s1, 3), "sum test error 3");
-    vector<float> v2 = {1, 2, 3, 4, 5, 6, 7};
-    float s2 = average(v2);
-    ensure(floatEqual(s2, 4), "sum test error 4");
-}
-
-float
-min1(vector<float> &array) {
-    float min = array[0];
-    size_t i = 1;
-    while(i < array.size()) {
-        float n = array[i];
-        if(n < min) {
-            min = n;
-        }
-        i = i + 1;
-    }
-    return min;
-}
-
-void
-testMin1(void) {
-    vector<float> v1 = {6, 3, 8, 4, 5};
-    float s1 = min1(v1);
-    ensure(floatEqual(s1, 3), "sum test error 3");
-    vector<float> v2 = {1, 2, 3, 4, 5, 6, 7};
-    float s2 = min1(v2);
-    ensure(floatEqual(s2, 1), "sum test error 4");
-}
-
-int
-sum1(int n) {
-    int s = 0;
-    size_t i = 1;
-    while (i < n + 1) {
-        if (i % 2 == 0) {
-            s = s - i;
-        } else {
-            s = s + i;
-        }
-        i = i + 1;
-    }
-    // log(s);
-    return s;
-}
-
-void
-testSum1(void) {
-    float s1 = sum1(5);
-    ensure(floatEqual(s1, 3), "sum test error 3");
-    float s2 = sum1(7);
-    ensure(floatEqual(s2, 4), "sum test error 4");
-}
-
-int
-sum2(int n) {
-    int s = 1;
-    size_t i = 2;
-    while (i < n + 1) {
-        if (i % 2 == 0) {
-            s = s + i;
-        } else {
-            s = s - i;
-        }
-        i = i + 1;
-        // log(s);
-    }
-    return s;
-}
-
-void
-testSum2(void) {
-    float s1 = sum2(5);
-    ensure(floatEqual(s1, -1), "sum test error 3");
-    float s2 = sum2(8);
-    ensure(floatEqual(s2, 6), "sum test error 4");
-}
-
-int
-fac1(int n) {
-    int s = 1;
-    size_t i = 1;
-    while (i < n + 1) {
-        s = s * i;
-        i = i + 1;
-        // log(s);
-    }
-    return s;
-}
-
-void
-testFac1(void) {
-    float s1 = fac1(5);
-    ensure(floatEqual(s1, 120), "sum test error 3");
-    float s2 = fac1(8);
-    ensure(floatEqual(s2, 40320), "sum test error 4");
-}
-
 void
 test() {
-    //testSum();
-    //testProduct();
-    // testabs1();
-    // testAverage();
-    // testMin1();
-    // testSum1();
-    // testSum2();
-    testFac1();
+    // testFind();
+    // testLowercase();
+    // testUppercase();
+    // testLowercase1();
+    // testUppercase1();
+    // testEncode1();
+    // testDecode1();
+    // testEncode2();
+    // testDecode2();
+    // testEncode3();
+    // testDecode3();
+    decode4();
 }
 
 int main(int argc, const char *argv[])
